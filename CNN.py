2cb7664afa19edc02a678b4c565bf05b4ce4c4ea
AugL0tus <jhaow42@gmail.com>
2024-08-08 01:48:56 +0800

    Add files via upload

---
+++ CNN.py
@@ -0,0 +1,42 @@
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from tensorflow.keras.utils import to_categorical
+
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+
+x_train = x_train.reshape((-1, 28, 28, 1))
+x_test = x_test.reshape((-1, 28, 28, 1))
+
+
+model = Sequential([
+    Conv2D(32, (3, 3), strides=(1, 1), activation='relu', input_shape=(28, 28, 1)),
+    MaxPooling2D((2, 2), strides=(2, 2)),
+    Conv2D(64, (3, 3), strides=(1, 1), activation='relu'),
+    MaxPooling2D((2, 2), strides=(2, 2)),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dropout(0.5),
+    Dense(10, activation='softmax')
+])
+
+
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+
+model.fit(x_train, y_train, epochs=5, batch_size=64, validation_data=(x_test, y_test))
+
+
+loss, accuracy = model.evaluate(x_test, y_test)
+print(f'Test accuracy: {accuracy}')
