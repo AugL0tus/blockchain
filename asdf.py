a3d344b81e1f3b18a7ad898c8e5237aebe8d85cb
wjh42 <wjh42h@qq.com>
2024-08-12 05:38:45 +0800

    add PoW

--- asdf.py
+++ asdf.py
@@ -1,6 +1,7 @@
 import hashlib
 import json
 from time import time
+from uuid import uuid4
 
 class Blockchain(object):
     def __init__(self):
@@ -8,6 +9,19 @@ class Blockchain(object):
 
         self.current_transactions = []
         self.new_block(previous_hash=1, proof=100)
+
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
 
     def new_block(self, proof, previous_hash=None):
         block = {
