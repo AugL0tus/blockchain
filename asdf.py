8222b4e5416e5230694e0cfc2594afd7ccfdd536
wjh42 <wjh42h@qq.com>
2024-08-17 15:15:01 +0800

    basic frame

--- asdf.py
+++ asdf.py
@@ -3,6 +3,9 @@ import json
 from time import time
 from uuid import uuid4
 
+from flask import Flask, jsonify, request
+
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
@@ -53,3 +56,56 @@ class Blockchain(object):
     def hash(block):
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
+
+app = Flask(__name__)
+
+node_identifier = str(uuid4()).replace('-', '')
+
+blockchain = Blockchain()
+
+@app.route('/mine', methods = ['GET'])
+def mine():
+    last_block = blockchain.last_block
+    last_proof = last_block['proof']
+    proof = blockchain.proof_of_work(last_proof)
+
+    blockchain.new_transactions(
+        sender="0",
+        recipient=node_identifier,
+        amount=1,
+    )
+
+    previous_hash = blockchain.hash(last_block)
+    block = blockchain.new_block(proof, previous_hash)
+
+    response = {
+        'message': "New Block Forged",
+        'index': block['index'],
+        'transaction': block['transaction'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash'],
+    }
+    return jsonify(response), 200
+
+@app.route('/transactions/new', methods = ['POST'])
+def new_transactions():
+    values = request.get_json()
+    required =  ['sender', 'recipient', 'amount']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    index = blockchain.new_transactions(values['sender'], values['recipient'], values['amount'])
+
+    response = {'message': f'Transaction will be added to Block{index}'}
+    return jsonify(response), 201
+
+@app.route('/chain', methods = ['GET'])
+def full_chain():
+    response = {
+        'chain': blockchain.chain,
+        'length': len(blockchain.chain),
+    }
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=500)
